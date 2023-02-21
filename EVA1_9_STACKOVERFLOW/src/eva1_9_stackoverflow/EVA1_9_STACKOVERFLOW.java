33ea08f795fa86a5d54183dff8e3643dda8a1912
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-02-21 09:46:21 -0600

    Add files via upload

---
+++ EVA1_9_STACKOVERFLOW/src/eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,12 @@
+package eva1_9_stackoverflow;
+///RODRIGO LEVI GARCIA GARAY /////
+public class EVA1_9_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        A();
+    }
+    
+}
