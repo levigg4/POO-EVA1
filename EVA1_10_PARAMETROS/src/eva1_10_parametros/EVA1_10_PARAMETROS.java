bf71c07708748a196035fe320501f7f0da59778f
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-02-21 09:51:27 -0600

    Add files via upload

---
+++ EVA1_10_PARAMETROS/src/eva1_10_parametros/EVA1_10_PARAMETROS.java
@@ -0,0 +1,14 @@
+package eva1_10_parametros;
+///RODRIGO LEVI GARCIA GARAY/////
+
+public class EVA1_10_PARAMETROS {
+    public static void main(String[] args) {
+        nprintln("Hola",6);
+    }
+    public static void nprintln(String message, int n){
+        for(int i=0; i<n; i++){
+            System.out.println(message);
+        }
+    }
+    
+}
