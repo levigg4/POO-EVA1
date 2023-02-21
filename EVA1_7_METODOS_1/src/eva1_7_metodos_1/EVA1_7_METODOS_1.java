806d9b582f8697c0d4f5ff6b46ec29051914ed1e
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-02-21 09:28:14 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,13 @@
+package eva1_7_metodos_1;
+///RODRIGO LEVI GARCIA GARAY/////
+
+public class EVA1_7_METODOS_1 {
+    
+    public static void main(String[] args) {
+        myMethod();//invocar funcion. llamada a función
+    }
+    public static void myMethod(){
+        System.out.println("You have called me: my name is myMethod!!");
+    }
+    
+}
