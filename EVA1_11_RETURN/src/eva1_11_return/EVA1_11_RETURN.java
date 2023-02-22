b590dff0ef7fc4465218c70f3f6ee69680bbfb4e
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-02-22 09:41:02 -0600

    Add files via upload

---
+++ EVA1_11_RETURN/src/eva1_11_return/EVA1_11_RETURN.java
@@ -0,0 +1,13 @@
+package eva1_11_return;
+//RODRIGO LEVI GARCIA GARAY ////
+public class EVA1_11_RETURN {
+
+    public static void main(String[] args) {
+        int ans;
+        ans = square(10);///inovacion o llamda a metodo
+        System.out.println("Resultado: "+ans);
+    }  
+    public static int square(int n){
+        return n*n;
+    }
+}
