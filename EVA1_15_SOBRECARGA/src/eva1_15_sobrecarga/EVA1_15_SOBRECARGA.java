c34a45f1f287ea34b6734881eff418ebaf88f3ff
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-02-23 09:50:12 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,16 @@
+package eva1_15_sobrecarga;
+//RODRIGO LEVI GARCIA GARAY 
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        /*diferentes PRINTLN: son 10
+        metodos, para imprimir diferentes tipos de datos*/
+        /*PARA CREAR SOBRE CARGA DE METODOS LA FIRMA DEL METODO 
+        DEBE SER UNICA*/
+        System.out.println("HOLA MUNDO");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    
+}
